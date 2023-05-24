3ddb009235e6627fe07291acfd69cedbee15faed
hamxamehboob <hamzamehboob2003@gmail.com>
2023-05-25 00:03:13 +0500

    Splash Screen Created

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:quiz_app/screens/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,7 +11,48 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    navigateToHome();
+  }
+
+  navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 4)).then(
+      (value) => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (_) => const LoginPage(),
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/splash_image.png'),
+              SizedBox(
+                height: size.height * .03,
+              ),
+              const Text(
+                "Quizzo",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: size.height * 0.1,
+              ),
+              const SpinKitCircle(
+                color: Colors.purple,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
