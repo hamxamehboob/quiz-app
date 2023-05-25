e4950f17136e71ae756bd7b4ca62a29efa14cfe8
hamxamehboob <hamzamehboob2003@gmail.com>
2023-05-25 21:55:09 +0500

    Firebase connected with Application

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:quiz_app/firebase_options.dart';
 import 'package:quiz_app/screens/splash_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  _initializeFirebase();
   runApp(const MyApp());
 }
 
@@ -14,4 +18,10 @@ class MyApp extends StatelessWidget {
       home: SplashScreen(),
     );
   }
+}
+
+_initializeFirebase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
